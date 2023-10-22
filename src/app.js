8f8b1fe03b54601cee72cd5c3a33e0abcf77959e
mrx7355608 <mrx7355608@gmail.com>
2023-10-22 11:29:44 +0500

    passport and sessions setup

--- src/app.js
+++ src/app.js
@@ -3,6 +3,10 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 import express from "express";
+import passport from "passport";
+import sessions from "express-session";
+import connectMongo from "connect-mongodb-session";
+import passportStrategySetup from "./passport.js";
 import { catch404, globalErrorHandler } from "./utils/errorHandlers.js";
 
 const app = express();
@@ -18,6 +22,25 @@ app.use(
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+// Express sessions setup
+const MongoStore = connectMongo(sessions);
+const mongoStore = new MongoStore({
+  collection: "sessions",
+  uri: process.env.DB_URL
+});
+app.use(sessions({
+  secret: process.env.SESSIONS_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 24 * 60 * 60 * 1000
+  },
+  store: mongoStore
+}));
+// Passport setup
+app.use(passport.initialize());
+app.use(passport.session());
+passportStrategySetup();
 
 // ERROR HANDLERS
 app.use(catch404);
---
+++ src/passport.js
@@ -0,0 +1,36 @@
+import passport from "passport";
+import { Strategy } from "passport-local";
+import usersDB from "./data/user.data.js";
+
+export default function passportStrategySetup() {
+  passport.use(
+    new Strategy({ usernameField: "email" }, async (email, password, done) => {
+      try {
+        const user = await usersDB.findByEmail(email);
+        if (user === false) {
+          return done(null, false);
+        }
+
+        const isValidPassword = await user.verifyPassword(password);
+        if (isValidPassword === false) {
+          return done(null, true, { message: "Incorrect email or password" });
+        }
+
+        return done(null, user);
+      } catch (err) {
+        return done(err);
+      }
+    })
+  );
+
+  passport.serializeUser((user, done) => done(null, user.id));
+
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const user = await usersDB.findById(id);
+      done(null, user);
+    } catch (err) {
+      done(err);
+    }
+  });
+}
